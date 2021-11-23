b9a2cce5957c545fd42e3940082af30ca39390d0
changmok <hong1071@nate.com>
2021-11-23 11:26:10 +0900

    babel

---
+++ 01. basic/ex05/src/index.js
@@ -0,0 +1,7 @@
+import ReactDom from 'react-dom';
+import {App} from './App.js';
+
+/* document
+    .getElementById('root')
+    .appendChild(App()); */
+ReactDom.render(App(), document.getElementById('root'));

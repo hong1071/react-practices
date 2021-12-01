de8a18f0898c2da3ad67409a493d8d3047468a0c
홍창목 <hong1071@nate.com>
2021-12-01 11:29:17 +0900

    제어 컴포넌트

--- 02.component/ex04/src/01/Incrementor01.js
+++ 02.component/ex04/src/01/Incrementor01.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 
-// 클래스형
+// 클래스형 컴포넌트에서 state 처리하는 방법
 
 export default class extends Component {
     constructor() {
         super(...arguments);
         this.state = {
-            step: this.props.step,
             val: this.props.begin
         }
+    }
+
+    onClickButton(e){
+        // this.state.value = this.state.value + this.props.step; 쓰지 않기
+        this.setState({
+            val: this.state.val + this.props.step
+        })
+        console.log(this.state.val + " " + this.props.step);
     }
 
     render() {
         return (
             <div>
-                <button onClick={(e) => {
-                    this.setState({
-                        val: this.state.val + this.state.step
-                    })
-                }}>
+                <button onClick={this.onClickButton.bind(this)}>
                     <strong>+</strong>
                 </button>
                 {' '}
                 <span>{this.state.val}</span>
+                {' '}
+                <button>
+                    <strong>-</strong>
+                </button>
             </div>
         );
     }
--- 02.component/ex04/src/01/Incrementor02.js
+++ 02.component/ex04/src/01/Incrementor02.js
@@ -1,20 +1,23 @@
 import React, {useState} from 'react';
 
-// 함수형
+// 함수형 컴포넌트에서 state 처리하는 방법
 
 export default function ({ begin, step }) {
     const [stateVal, setVal] = useState(begin);
-    const [stateStep] = useState(step);
 
     return (
         <div>
             <button onClick={(e) => {
-                setVal(stateVal + stateStep);
+                setVal(stateVal + step);
             }}>
                 <strong>+</strong>
             </button>
             {' '}
             <span>{ stateVal }</span>
+            {' '}
+            <button>
+                <strong>-</strong>
+            </button>
         </div>
     );
 }

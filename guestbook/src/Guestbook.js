1ee4099c27d396e6ff1dc298b52179e445f58407
홍창목 <hong1071@nate.com>
2021-12-03 10:36:36 +0900

    guestbook start

--- guestbook/src/Guestbook.js
+++ guestbook/src/Guestbook.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import WriteForm from './WriteForm';
+import MessageList from './MessageList';
+import styles from './assets/scss/Guestbook.scss';
 
+import data from './assets/json/data.json';
 
-export default function(){
-    return(
-        <div className='guestbook'>
-           <h1>Guestbook</h1>
+export default function Guestbook() {
+    const [messages, setMessages] = useState(data);
+
+    return (
+        <div className={styles.ScrollOuter}>
+            <div>
+                <div className={styles.Guestbook}>
+                    <h1>방명록</h1>
+                    <WriteForm/>
+                    <MessageList messages={messages}/>
+                </div>
+            </div>
         </div>
-    )
+    );
 }

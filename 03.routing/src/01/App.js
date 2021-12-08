40ff1f97d589a49e1a306013ca93917bc1894235
홍창목 <hong1071@nate.com>
2021-12-08 10:39:45 +0900

    router

---
+++ 03.routing/src/01/App.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import Main from "./component/Main";
+import Gallery from "./component/Gallery";
+import Guestbook from "./component/Guestbook";
+
+export default function App() {
+
+    const [route, setRoute] = useState('');
+
+    useEffect(() => {
+        
+        const handleHashChange = () => setRoute(window.location.hash.substr(1));
+
+        window.addEventListener('hashchange', handleHashChange);
+        
+        return () => {
+            window.removeEventListener('hashchange', handleHashChange)
+        };
+    }, []);
+
+    return (() => {
+        switch(route){
+            case '/':
+                return <Main />;
+            case '/guestbook':
+                return <Guestbook />;
+            case '/gallery':
+                return <Gallery />;
+            default:
+                return null;
+        }
+        return <div>{`${route}에 맞는 컴포넌트를 리턴합니다`}</div>
+    })();
+}
